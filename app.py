0fe890595d3ea755ef574d0dff0af3e4bc87e9cc
Swanchick <61909965+Swanchick@users.noreply.github.com>
2023-02-18 18:47:27 +0100

    Show players that are playing on server right now

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from valve.source.a2s import ServerQuerier
+from valve.source.messages import PlayerEntry
 
 app = Flask(__name__)
+server = ServerQuerier(("91.203.5.123", 25016), 5)
 
 @app.route("/")
 def main():
-    return render_template("main.html")
+	playersInfo = server.players()
+	players = []
+
+	for player in playersInfo["players"]:
+		players.append(player["name"])
+
+	return render_template("main.html", players=players)
 
 if __name__ == "__main__":
-    app.run(port=50000, debug=True)
+	app.run(port=50000, debug=True)
